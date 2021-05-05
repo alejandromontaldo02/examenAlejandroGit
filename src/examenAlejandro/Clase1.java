59e9fa217af39933544b0863ebd53b2b62c6a1d7
alejandromontaldo02 <alejandromontaldo02@gmail.com>
2021-05-05 10:53:03 +0200

    Clase 1 con exito

---
+++ src/examenAlejandro/Clase1.java
@@ -0,0 +1,9 @@
+package examenAlejandro;
+
+public class Clase1 {
+
+	public Clase1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/examenAlejandroGit/clase1Git.java
@@ -0,0 +1,17 @@
+package examenAlejandroGit;
+
+import java.util.Scanner;
+
+public class clase1Git {
+
+	public clase1Git() {
+		// TODO Auto-generated constructor stub
+		Scanner leer = new Scanner(System.in);
+		int x=1;
+		System.out.println(x);
+	}
+
+	
+			
+
+}
