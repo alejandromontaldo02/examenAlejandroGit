4c3e3aaaa721fc4022eee1bbf7dedafc05080a98
alejandromontaldo02 <alejandromontaldo02@gmail.com>
2021-05-05 11:06:38 +0200

    commit 3

--- src/examenAlejandro/Clase1.java
+++ src/examenAlejandro/Clase1.java
@@ -1,9 +1,13 @@
 package examenAlejandro;
+import java.util.Scanner;
+import java.util.Scanner;
 
 public class Clase1 {
 
 	public Clase1() {
 		// TODO Auto-generated constructor stub
 	}
-
+	Scanner leer = new Scanner(System.in);
+	int x=1;
+	System.out.println(x);
 }
